1d5f41aca0a315587b4d2e04aa44464874dbbfe0
Mateusz <298881644+matmrozdev@users.noreply.github.com>
2026-08-28 16:15:58 +0200

    feat(mobile): integrate approved brand assets

---
+++ apps/mobile/src/config/brand-assets.ts
@@ -0,0 +1,4 @@
+export const brandAssets = {
+  appIcon: require('../../assets/images/brand/reward-goals-icon.png'),
+  splash: require('../../assets/images/brand/reward-goals-splash.png'),
+} as const;
